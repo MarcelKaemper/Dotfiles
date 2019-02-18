3efbd11cb664e2d19f124163b8074ff7fa09699c
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-18 14:36:52 +0100

    Added stuff I don't remember

--- backup.sh
+++ backup.sh
@@ -1,8 +1,19 @@
 #!bin/shell
 echo Loading date
 today=`date '+%Y_%d_%m_%H_%M_%S'`;
-echo got date $today
-echo Enter path to copy:
-read filesToCopy
+echo $today
+file=./backups/path
+if [ -e $file ]
+then
+	read -r filesToCopy<$file
+else
+	touch $file
+	echo $file
+	echo Enter path to copy:
+	read path 
+	echo $path > $file
+	read -r filesToCopy<$file
+fi
+
 tar -czf ./backups/$today.tar.gz $filesToCopy 
 echo Copying done.
--- refresh.sh
+++ refresh.sh
@@ -3,7 +3,7 @@ echo "Loading files..."
 cp ~/.config/i3/config i3.conf
 cp ~/.config/i3status/config i3status.conf
 cp ~/.config/conky/conky.conf conky.conf
-cp ~/Documents/changeMon.sh changeMon.sh
+cp ~/changeMon.sh changeMon.sh
 cp ~/Documents/coding/projects/backup.sh backup.sh
 cp ~/.vimrc .vimrc
 echo "...Done!"
